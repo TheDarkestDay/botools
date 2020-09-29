ccb2617bb3c422d7c1f3bd6e7e7f39160360454e
Aleksandr Brenchev <abrenchev@griddynamics.com>
2020-09-29 16:33:59 +0200

    Removed secrets and bot credentials

--- src/main/java/com/abrenchev/App.java
+++ src/main/java/com/abrenchev/App.java
@@ -3,6 +3,6 @@ package com.abrenchev;
 public class App {
     public static void main(String... args) {
         var botools = new Botools();
-        botools.runBot("1249193371:AAFtI7jGWjA428qng8zw61Wm9die4UKRWGY", "@botoolsSample_bot", ExampleBot.class);
+        botools.runBot("your-secret-key", "your-bot-name", ExampleBot.class);
     }
 }
